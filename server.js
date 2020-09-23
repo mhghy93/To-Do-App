1b47d3ac32b41c9106809e69c1a494e81c1d75f7
mustaque <hussainmustaque98407@gmail.com>
2020-09-24 01:38:00 +0530

    init

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const connectDB = require('./config/db');
+
+const app = express();
+
+// connect database
+connectDB();
+
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => res.send('API running'));
+
+// Define routes
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));

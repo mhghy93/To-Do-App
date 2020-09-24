ffd3e6ba6cc514fbd1c67ef7f4b8317f96586772
mustaque <hussainmustaque98407@gmail.com>
2020-09-24 20:20:02 +0530

    todo controller set up

---
+++ controllers/todo.js
@@ -0,0 +1,13 @@
+const ToDo = require('../models/todo');
+
+exports.showToDos = (req, res) => {};
+
+exports.showToDosInProgress = (req, res) => {};
+
+exports.showToDosCompleted = (req, res) => {};
+
+exports.createToDo = (req, res) => {};
+
+exports.editToDo = (req, res) => {};
+
+exports.deleteToDo = (req, res) => {};

46b4945ebce34bf2de8f48c79eaa790ef2f76b9a
mustaque <hussainmustaque98407@gmail.com>
2020-09-30 15:08:28 +0530

    add initial state

---
+++ client/src/context/todo/ToDoState.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import axios from 'axios';
+import ToDoContext from './ToDoContext';
+import {
+  SHOW_TODOS,
+  SHOW_TODOS_IN_PROGRESS,
+  SHOW_TODOS_COMPLETED,
+  MOVE_TODO_IN_PROGRESS,
+  COMPLETE_TODO,
+  CREATE_TODO,
+  EDIT_TODO,
+  DELETE_TODO,
+} from '../types';
+
+const ToDoState = (props) => {
+  const initialState = {};
+};
+
+export default ToDoState;

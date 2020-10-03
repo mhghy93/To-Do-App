2ddd9934a1284dabbee06c4c6ce848912cf9bb61
mustaque <hussainmustaque98407@gmail.com>
2020-10-03 22:19:30 +0530

    component cleanup

--- client/src/components/todo/ToDoItem.js
+++ client/src/components/todo/ToDoItem.js
@@ -1,7 +1,28 @@
-import React, { Fragment } from 'react';
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles({
+  root: {
+    minWidth: 275,
+    marginTop: 30,
+  },
+});
 
 const ToDoItem = () => {
-  return <Fragment>ToDos</Fragment>;
+  const classes = useStyles();
+
+  return (
+    <Card className={classes.root} variant="outlined">
+      <CardContent>
+        <Typography variant="body2" component="p">
+          ToDo
+        </Typography>
+      </CardContent>
+    </Card>
+  );
 };
 
 export default ToDoItem;
--- client/src/components/todo/ToDos.js
+++ client/src/components/todo/ToDos.js
@@ -7,11 +7,6 @@ const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
   },
-  paper: {
-    padding: theme.spacing(2),
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
-  },
 }));
 
 const Todos = () => {

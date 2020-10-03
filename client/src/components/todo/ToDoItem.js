136a1c7db2b4c35efcebcd444480349dd96886df
mustaque <hussainmustaque98407@gmail.com>
2020-10-03 22:34:08 +0530

    modify todoitem ui

--- client/src/components/todo/ToDoItem.js
+++ client/src/components/todo/ToDoItem.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
 
 const useStyles = makeStyles({
   root: {
@@ -17,9 +19,21 @@ const ToDoItem = () => {
   return (
     <Card className={classes.root} variant="outlined">
       <CardContent>
-        <Typography variant="body2" component="p">
-          ToDo
-        </Typography>
+        <Grid container spacing={1}>
+          <Grid item xs={12} sm={6}>
+            <Typography variant="body2" component="p">
+              ToDo
+            </Typography>
+          </Grid>
+          <Grid item xs={12} sm={3}>
+            <Button variant="contained">Edit</Button>
+          </Grid>
+          <Grid item xs={12} sm={3}>
+            <Button variant="contained" color="secondary">
+              Delete
+            </Button>
+          </Grid>
+        </Grid>
       </CardContent>
     </Card>
   );
